92089bbf980c587e7e49bcdbec3f124fd0f4f638
GarnikR <g.rushanyan@yahoo.com>
2023-09-22 21:39:43 +0400

    include_SG

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,36 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow inbound traffic ssh"
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  }
+
 resource "aws_instance" "web" {
   instance_type = "t2.micro"
   ami = "ami-0fc5d935ebf8bc3bc"
-
-  }
+  key_name      = aws_key_pair.ssh_key.key_name
+ }
 
