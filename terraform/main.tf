7955bc305bcb747139730c1639f5265c6056debb
GarnikR <g.rushanyan@yahoo.com>
2023-09-24 14:59:08 +0400

    SG

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,6 +42,11 @@ resource "aws_security_group" "allow_ssh" {
   }
   }
 
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.allow_ssh.id
+  network_interface_id = aws_instance.web.primary_network_interface_id
+}
+
 resource "aws_instance" "web" {
   instance_type = "t2.micro"
   ami = "ami-0fc5d935ebf8bc3bc"
