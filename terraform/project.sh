0d038f988d66339b5e50cc2c3e1b86c3299f2730
GarnikR <g.rushanyan@yahoo.com>
2023-09-27 18:47:50 +0400

    with_script

---
+++ terraform/project.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "step_1"
+terraform output | grep EIP | awk -F'"' '/"/ {print $2}' >> ../ansible/inventory
+echo "step_2"
+rds_address=$(terraform output | grep RDS_IP | awk -F'"' '/"/ {print $2}')
+echo "step_3"
+ansible-playbook ../ansible/book1.yaml -i ../ansible/inventory -e mysql_host=$rds_address #--ssh-common-args='-o StrictHostKeyChecking=no'
+
