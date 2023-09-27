b30186caa91a0d5c30603d31c90cad4adb58fa3c
GarnikR <g.rushanyan@yahoo.com>
2023-09-27 20:14:39 +0400

    20:14

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,6 @@ resource "aws_security_group" "allow_ssh" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-
   egress {
     from_port        = 0
     to_port          = 0
--- terraform/project.sh
+++ terraform/project.sh
@@ -5,5 +5,8 @@ terraform output | grep EIP | awk -F'"' '/"/ {print $2}' >> ../ansible/inventory
 echo "step_2"
 rds_address=$(terraform output | grep RDS_IP | awk -F'"' '/"/ {print $2}')
 echo "step_3"
-ansible-playbook ../ansible/book1.yaml -i ../ansible/inventory -e mysql_host=$rds_address #--ssh-common-args='-o StrictHostKeyChecking=no'
+terraform output | grep RDS_IP | awk -F'"' '/"/ {print $2}' >> ./testoutput
+
+echo "step_4"
+ansible-playbook ../ansible/book1.yaml -i ../ansible/inventory -e mysql_host=$rds_address --ssh-common-args='-o StrictHostKeyChecking=no'
 
